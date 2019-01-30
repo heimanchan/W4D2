41868a71dadc6ecb657c3fbfa6461ff64ed10c4d
heimanchan <heimanchan0@gmail.com>
2019-01-29 16:21:21 -0800

    Add form for create and edit

--- cats99/app/controllers/cats_controller.rb
+++ cats99/app/controllers/cats_controller.rb
@@ -1,10 +1,42 @@
 class CatsController < ApplicationController
+  def create
+    cat = Cat.new(cat_params)
+
+    if cat.save
+      redirect_to cats_url
+    else
+      render cat.errors.full_messages
+    end
+  end
+  
   def index
-    @kitty = Cat.all
+    @cat = Cat.all
   end
 
   def show
-    @kitty = Cat.find(params[:id])
+    @cat = Cat.find(params[:id])
+  end
+
+  def new
+    render :new
+  end
+
+  def update  #patch
+    @cat = Cat.update(params[:id], cat_params)
+    if @cat.save
+      redirect_to cat_url
+    else
+      render @cat.errors.full_messages
+    end
+  end
+  
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end
 
 end
--- cats99/app/models/cat.rb
+++ cats99/app/models/cat.rb
@@ -1,9 +1,11 @@
 class Cat < ApplicationRecord
   include ActionView::Helpers::DateHelper
 
+  COLORS = ["white", "black", "brown", "orange", "yellow", "grey", "green"]
+
   validates :birth_date, :color, :name, :sex, :description, presence: true
   validates :sex, inclusion: { in: ["M", "F"], message: "Has to be M or F"}
-  validates :color, inclusion: { in: ["white", "black", "brown", "orange", "yellow", "grey", "green"]}
+  validates :color, inclusion: { in: COLORS }
 
 
   def age
